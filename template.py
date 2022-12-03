7b0c14b60f54af811f7f55c3289c1e21423898ae
Keith Long <keith.long@appfolio.com>
2022-12-03 11:38:41 -0800

    Template without logging

--- template.py
+++ template.py
@@ -1,30 +1,18 @@
 import requests
-import logging
 import os
 
-def setup_request_logs():
-    import http.client as http_client
-    http_client.HTTPConnection.debuglevel = 1
-
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.DEBUG)
-    requests_log = logging.getLogger("requests.packages.urllib3")
-    requests_log.setLevel(logging.DEBUG)
-    requests_log.propagate = True
-
-def solve(input_):
+def part1(input_):
+    for line in input_.split('\n'):
+        print(line)
     return input_
 
 try:
-    #setup_request_logs()
-
-    # name the files after the day. ie. 1.py for day 1
     day = os.path.basename(__file__).split('.')[0]
     url = 'https://adventofcode.com/2022/day/'+day+'/input'
     cookies = {'session': os.environ['ADVENT_TOKEN']}
     r = requests.get(url, cookies=cookies)
 
-    print(solve(r.text))
+    print(part1(r.text))
 
 except e:
     print(e)

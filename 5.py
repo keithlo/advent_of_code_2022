cf970b65f09e2093ceb227bc7ab9beeb3d640b69
Keith Long <keith.long@appfolio.com>
2022-12-05 18:33:03 -0800

    Day 5

--- 5.py
+++ 5.py
@@ -1,28 +1,6 @@
 import requests
 import os
 
-'''
-Sets up stacks using a matrix.
-'''
-def setupStacks(text) -> [[str]]:
-    stacks_block = []
-    for line in text:
-        if line[0] == '[': stacks_block.append(line)
-        else: break
-
-    stacks = [[]]
-    depth = len(stacks_block)
-    for i, char in enumerate(line):
-        if char.isnumeric():
-            j = depth-1
-            stack = []
-            while j >= 0:
-                cargo = stacks_block[j][i]
-                if cargo.isalpha(): stack.append(cargo)
-                j -= 1
-            stacks.append(stack)
-    return stacks
-
 def setupStacks(text) -> [[str]]:
     stack_count = len(text[0]) // 4 + 2
     stacks = [[] for _ in range(stack_count)]
